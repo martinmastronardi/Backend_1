a34be2ee4b3c6848c512b28521e84c04865d206f
Martin_Mastronardi <mastronardi390@gmail.com>
2024-07-01 13:28:03 -0300

    3

--- Martin_Mastronardi_Entrega_1/src/app.js
+++ Martin_Mastronardi_Entrega_1/src/app.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
     res.send('Esto fue enviado desde el servidor')
 })
 
+// guarda querys
+app.get('/usuarios', (req, res) => {
+    console.log(req.query)
+    res.send('usuarios')
+})
+
+// guarda parametros
+app.get('/usuarios/:id', (req, res) => {
+    console.log(req.params.id)
+    res.send('params')})
+
+app.get('/productos', (req, res) => {
+    res.send('productos')
+})
 
 app.listen(8080, () => 
     console.log('Servidor levantado'))
